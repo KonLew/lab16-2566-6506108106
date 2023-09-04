da3630794bd9246f280cc3e206aedad1165ee710
surangrat teymeesak <surangrat231@gmail.com>
2023-09-04 15:47:28 +0700

    com

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -1,5 +1,6 @@
 import { DB } from "@/app/libs/DB";
 import {
+  zStudentDeleteBody,
   zStudentGetParam,
   zStudentPostBody,
   zStudentPutBody,
@@ -29,8 +30,10 @@ export const GET = async (request) => {
   if (program !== null) {
     filtered = filtered.filter((std) => std.program === program);
   }
-
   //filter by student id here
+  if (studentId !== null) {
+    filtered = filtered.filter((std) => std.studentId === studentId);
+  }
 
   return NextResponse.json({ ok: true, students: filtered });
 };
@@ -98,9 +101,41 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
-
+  const body = await request.json();
+  const parseResult = zStudentDeleteBody.safeParse(body);
   //check if student id exist
-
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === body.studentId
+  );
+  if (parseResult.success === false) {
+    //ส่ง error
+    return NextResponse.json(
+      {
+        ok: false,
+        message: parseResult.error.issues[0].message,
+      },
+      {
+        status: 400,
+      }
+    );
+  } else {
+    if (foundIndex >= 0) {
+      DB.students.splice(foundIndex, 1);
+      return NextResponse.json({
+        ok: true,
+        message: `Student Id ${body.studentId} has been deleted`,
+      });
+    }
+    if (foundIndex === -1) {
+      return NextResponse.json(
+        {
+          ok: false,
+          message: "Student ID does not exist",
+        },
+        { status: 404 }
+      );
+    }
+  }
   //perform removing student from DB. You can choose from 2 choices
   //1. use array filter method
   // DB.students = DB.students.filter(...);
@@ -108,8 +143,8 @@ export const DELETE = async (request) => {
   //or 2. use splice array method
   // DB.students.splice(...)
 
-  return NextResponse.json({
-    ok: true,
-    message: `Student Id xxx has been deleted`,
-  });
+  // return NextResponse.json({
+  //   ok: true,
+  //   message: `Student Id xxx has been deleted`,
+  // });
 };
